98083d72ce118fda2e46e9a6c439ea9ebf892e68
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-07-28 01:28:26 +0200

    Work in progress

--- jobrunner-entrypoint.sh
+++ jobrunner-entrypoint.sh
@@ -16,8 +16,8 @@ kill_runner() {
 trap kill_runner SIGTERM
 
 while true; do
-	if [ -e /shared/LocalSettings.php ]; then
-		php maintenance/runJobs.php --wait --maxjobs="$MAX_JOBS" --conf /shared/LocalSettings.php &
+	if [ -e LocalSettings.php ]; then
+		php maintenance/runJobs.php --wait --maxjobs="$MAX_JOBS" --conf LocalSettings.php &
 		PID=$!
 		wait "$PID"
 	else
