eb8158481a7aeb3874285b1111e32769242eabb6
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-07-28 01:24:51 +0200

    Minor

--- jobrunner-entrypoint.sh
+++ jobrunner-entrypoint.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-# This file is provided by the wikibase/wikibase docker image.
 
-# Test if required environment variables have been set
-REQUIRED_VARIABLES=(MW_ADMIN_NAME MW_ADMIN_PASS MW_ADMIN_EMAIL MW_WG_SECRET_KEY DB_SERVER DB_USER DB_PASS DB_NAME)
-for i in "${REQUIRED_VARIABLES[@]}"; do
-    eval THISSHOULDBESET=\$"$i"
-    if [ -z "$THISSHOULDBESET" ]; then
-    echo "$i is required but isn't set. You should pass it to docker. See: https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file";
-    exit 1;
-    fi
-done
-
-set -eu
+# Originally inspired by Brennen Bearnes jobrunner entrypoint
+# https://gerrit.wikimedia.org/r/plugins/gitiles/releng/dev-images/+/refs/heads/master/common/jobrunner/entrypoint.sh
 
 # Wait for the db to come up
 /wait-for-it.sh "$DB_SERVER" -t 300
@@ -20,25 +10,18 @@ set -eu
 sleep 1
 /wait-for-it.sh "$DB_SERVER" -t 300
 
-# Do the mediawiki install (only if LocalSettings doesn't already exist)
-if [ ! -e "/var/www/html/LocalSettings.php" ]; then
-    php /var/www/html/maintenance/install.php --dbuser "$DB_USER" --dbpass "$DB_PASS" --dbname "$DB_NAME" --dbserver "$DB_SERVER" --lang "$MW_SITE_LANG" --pass "$MW_ADMIN_PASS" "$MW_SITE_NAME" "$MW_ADMIN_NAME"
-    php /var/www/html/maintenance/resetUserEmail.php --no-reset-password "$MW_ADMIN_NAME" "$MW_ADMIN_EMAIL"
+kill_runner() {
+	kill "$PID" 2> /dev/null
+}
+trap kill_runner SIGTERM
 
-    # Copy our LocalSettings into place after install from the template
-    # https://stackoverflow.com/a/24964089/4746236
-    export DOLLAR='$'
-    envsubst < /LocalSettings.php.template > /var/www/html/LocalSettings.php
-
-    # Run update.php to install Wikibase
-    php /var/www/html/maintenance/update.php --quick
-
-    # Run extrascripts on first run
-    if [ -f /extra-install.sh ]; then
-        # shellcheck disable=SC1091
-        source /extra-install.sh
-    fi
-fi
-
-# Run the actual entry point
-docker-php-entrypoint apache2-foreground
+while true; do
+	if [ -e /shared/LocalSettings.php ]; then
+		php maintenance/runJobs.php --wait --maxjobs="$MAX_JOBS" --conf /shared/LocalSettings.php &
+		PID=$!
+		wait "$PID"
+	else
+		echo "LocalSettings.php not shared yet - waiting for 10 seconds."
+		sleep 10
+	fi
+done
